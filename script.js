469cc1ee2c567fed1443676654a522a0b08def68
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-07-25 11:59:16 +0200

    Update script.js
    Removing console.log from script.js file

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ const itemLists = document.querySelector('.items');
 const deleteAll = document.querySelector('#clear');
 const inputValue = document.querySelector('.form-input').value;
 const filter = document.getElementById('filter');
-// Load items from localStorage
 const store = JSON.parse(localStorage.getItem('store')) || [];
 
 function onSubmit(e) {
   e.preventDefault();
   const inputValue = document.querySelector('.form-input').value;
-
   if (inputValue === '') {
     alert('Please add an item');
     return;
@@ -23,8 +21,6 @@ function onSubmit(e) {
   localStorage.setItem('item', inputValue);
   localStorage.setItem('store', JSON.stringify(store));
 
-  console.log(localStorage.getItem('store'));
-
   i.className = 'fa-solid fa-xmark';
   btn.appendChild(i);
   btn.className = 'remove-item btn-link text-red';
@@ -34,13 +30,11 @@ function onSubmit(e) {
   document.querySelector('.form-input').value = '';
 }
 
-console.log(store);
 function deleteSingleElement(e) {
   if (e.target.parentElement.classList.contains('remove-item')) {
     e.target.parentElement.parentElement.remove();
   }
 }
-// delete simple item
 
 function clearList() {
   itemLists.innerHTML = '';
@@ -52,7 +46,6 @@ function filterItems(searchText) {
 
   items.forEach((item) => {
     const itemText = item.firstChild.textContent.toLowerCase();
-
     if (itemText.includes(searchText.toLowerCase())) {
       item.style.display = 'block';
     } else {
@@ -60,6 +53,7 @@ function filterItems(searchText) {
     }
   });
 }
+
 form.addEventListener('submit', onSubmit);
 filter.addEventListener('input', (e) => filterItems(e.target.value));
 itemLists.addEventListener('click', deleteSingleElement);
